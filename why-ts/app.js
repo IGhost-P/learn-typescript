e53227ba095fa9ce9962caafb2527ae1f9ebb60f
IGhos-P <dndb3599@gmail.com>
2021-08-18 16:38:53 +0900

    ts 시작

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,6 +9,11 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * jsdoc을 이용해서도 타입을 정할수는 있으나, 오류 검사를 해주지진 않는다 => ts-check기능을 를 이용해야함
+ * @typedef {object} Address
+ * @property {string} street
+ */
 function startApp() {
   axios
     .get(url)
